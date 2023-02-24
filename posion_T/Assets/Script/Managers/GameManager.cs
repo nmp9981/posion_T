5d614307a075c23f50421d652f58401c2a7ca4d7
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2023-02-24 16:38:06 +0900

    박성택

---
+++ posion_T/Assets/Script/Managers/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    static GameManager _instance;
+    DataManager _dataManager = new DataManager();
+    InputManager _inputManager = new InputManager();
+    SoundManager _soundManager = new SoundManager();
+
+
+    public static GameManager Instance { get { init(); return _instance; } }
+    public static InputManager Input { get { return Instance._inputManager; } }
+    public static DataManager Data { get { return Instance._dataManager; } }
+    public static SoundManager Sound { get { return Instance._soundManager; } }
+
+
+
+    static void init()
+    {
+        if(_instance == null)
+        {
+            GameObject gm = GameObject.Find("GameManager");
+            if(gm == null)
+            {
+                gm = new GameObject { name = "GameManager" };
+                DontDestroyOnLoad(gm);
+                gm.AddComponent<GameManager>();
+            }
+
+            _instance = gm.GetComponent<GameManager>();
+            Sound.init();
+
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input.OnUpdate();   
+    }
+}
---
+++ posion_T/Assets/Script/Managers/SoundManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager 
+{
+    AudioSource[] audioSource = new AudioSource[(int)Define.Sound.MaxCound];     //  1 번은 BGM 2번은 Effect 3번은 UI사운드
+    Dictionary<string, AudioClip> EffectSound = new Dictionary<string, AudioClip>(); 
+
+
+    public void init()
+    {
+        GameObject Sound = GameObject.Find("Sound");
+        if(Sound == null)
+        {
+            Sound = new GameObject{ name = "Sound"};
+            Object.DontDestroyOnLoad(Sound);
+            string[] soundNames = System.Enum.GetNames(typeof(Define.Sound));
+
+            for (int i = 0; i < (int)Define.Sound.MaxCound; i++)
+            {
+                GameObject sound = new GameObject { name = soundNames[i]};
+                
+                sound.transform.parent = Sound.transform;
+            }
+            
+
+
+        }
+    }
+    
+}
